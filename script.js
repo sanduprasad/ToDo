00e89e9e2441a248650e649c38abde47ddd4ccd1
unknown <sanduprasad>
2018-07-05 10:14:48 +0530

    js updated

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-$("ul").on("click", "li", function() {
-  $(this).toggleClass("completed");
-});
 
-$("ul").on("click", "span", function(event) {
-  $(this)
-    .parent()
-    .fadeOut(500, function() {
-      $(this).remove();
-    });
-  event.stopPropagatiion();
-});
+$(document).ready(function(){
+  $("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+  });
 
-$("input").keypress(function(event) {
-  if (event.which === 13) {
-    var todoText = $(this).val();
-    $(this).val("");
-    $("ul").append("<li><span>Delete </span>" + todoText + "</li>");
-  }
+  $("ul").on("click", "span", function(event) {
+    $(this)
+      .parent()
+      .fadeOut(500, function() {
+        $(this).remove();
+      });
+    event.stopPropagatiion();
+  });
+
+  $("input").keypress(function(event) {
+    if (event.which === 13) {
+      var todoText = $(this).val();
+      $(this).val("");
+      $("ul").append("<li><span>Delete </span>" + todoText + "</li>");
+    }
+  });
 });
